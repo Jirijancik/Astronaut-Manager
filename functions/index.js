f1f4096aeb4d2712aefc2a35efc2f533a7011651
jirijancik <jirijancik2@gmail.com>
2019-12-21 00:52:55 +0100

    small-update-to-coud-functions

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,34 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const cors = require('cors')({ origin: true });
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.getAstronauts = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('managedAstronauts').get().then(data => {
-        let managedAstronauts = [];
-        data.forEach(doc => {
-            managedAstronauts.push(doc.data());
-        });
-        return res.jsonp(managedAstronauts);
+exports.getAstronauts = functions.region("europe-west1").https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        admin
+            .firestore()
+            .collection('managedAstronauts')
+            .get()
+            .then(data => {
+                let managedAstronauts = [];
+                data.forEach(doc => {
+                    managedAstronauts.push({
+                        astronautId: doc.id,
+                        lastName: doc.data().lastName,
+                        firstName: doc.data().firstName,
+                        superpower: doc.data().superpower,
+                        dateOfBirth: doc.data().dateOfBirth
+                    });
+                });
+                return res.json(managedAstronauts);
+            }).catch((err) => console.log(err));
     })
-    .catch((err) => console.log(err));
 });
 
-exports.createAstronaut = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).jsonp({error: "Method not allowed!"});
+
+exports.createAstronaut = functions.region("europe-west1").https.onRequest((req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(400).json({ error: "Method not allowed!" });
     }
 
     const newAstronaut = {
@@ -37,10 +42,10 @@ exports.createAstronaut = functions.https.onRequest((req, res) => {
         .collection('managedAstronauts')
         .add(newAstronaut)
         .then(doc => {
-            res.json({message: `Astronaut ${newAstronaut.firstName} ${newAstronaut.lastName} was added!`});
+            res.json({ message: `Astronaut ${newAstronaut.firstName} ${newAstronaut.lastName} was added!` });
         })
         .catch(err => {
-            res.status(500).json({ error: "Something Went Wrong =("});
+            res.status(500).json({ error: "Something Went Wrong =(" });
             console.log(err);
         })
 })
